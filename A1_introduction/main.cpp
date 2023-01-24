81e8bc97cdfcff713fbbc0e659f22972bc1e3a3b
hema8Codes <developer8hema@gmail.com>
2023-01-24 21:19:23 +0530

    first commit

---
+++ A1_introduction/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello Everyone!"<<"\n";
+    float n1, n2;
+    cout<<"\n The first number: ";
+    cin>>n1;
+    cout<<"\n The second number: ";
+    cin>>n2;
+
+    cout<<"Total sum := "<<n1 +n2<<"\n";
+    return 0;
+}
