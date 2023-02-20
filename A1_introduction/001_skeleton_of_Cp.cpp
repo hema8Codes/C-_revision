dad8a84921df578f45b01cf3a7009158ee0aff77
hema8Codes <developer8hema@gmail.com>
2023-02-20 13:12:41 +0530

    updated

---
+++ A1_introduction/001_skeleton_of_Cp.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+// #include -> file inclusion directive
+// Tells the preprocessor to include the contents of a specified file at the point where the directive appears.
+//<iostream> -> library
+using namespace std;
+
+int main(){
+    cout<<"\n Hello World";
+    return 0;
+}
